1414ff94c1d5c7d12e80bfc93326d24eeb23867a
Sam Vanderstraeten <sam.vanderstraeten@pxl.be>
2020-09-25 22:11:07 +0200

    command research

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
 MAX_ANGLE = 0.7
 MAX_THROTTLE = 0.27
 LOCATION = "inside"
+
+# command to write this stuff to file in one line:
+# (echo "ANGLE=0.3" ; echo "THROTTLE=0.5" ; echo "LOCATION='thuis'") > /home/sam/test.py
